d3a4496945f2ccbff5950916ddcfbd692eae6d4f
unknown <whobeautiful@163.com>
2022-08-03 23:00:22 +0800

    'supermall初次提交到github'

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser', 
+    "requireConfigFile": false
+  },
+  rules: {
+    'indent': ['error', 2],
+    'no-multiple-empty-lines': [2, { //空行最多不能超过1行
+      'max': 1
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/multi-word-component-names": "off",
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
